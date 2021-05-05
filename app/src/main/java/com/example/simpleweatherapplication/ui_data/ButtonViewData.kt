d4c1d2fc3c33843b49e8873e0ddc0229328647fb
Stavros Georgiou <stavris.8894@gmail.com>
2021-05-05 19:26:21 +0300

    remove ViewHolderGenerator

--- app/src/main/java/com/example/simpleweatherapplication/ui_data/ButtonViewData.kt
+++ app/src/main/java/com/example/simpleweatherapplication/ui_data/ButtonViewData.kt
@@ -1,6 +1,7 @@
 package com.example.simpleweatherapplication.ui_data
 
 import com.example.simpleweatherapplication.utils.RecyclerViewItemTypes
+import cy.com.core.interfaces.RecyclerViewItemType
 import cy.com.core.interfaces.RecyclerViewUiItem
 import cy.com.core.interfaces.ViewHolderId
 
@@ -8,7 +9,7 @@ class ButtonViewData<T : ViewHolderId>(
     id: T,
     val title: String
 ) : RecyclerViewUiItem<T>(id) {
-    override val itemType: RecyclerViewItemTypes
+    override val itemType: RecyclerViewItemType
         get() = RecyclerViewItemTypes.BUTTON_VIEW
     override val data: Any
         get() = title
